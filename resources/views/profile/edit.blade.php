05d623b1b50dfadb8cf251e5a21992b875cf4f9d
Zeeshan Chawdhary <imzeeshanc@gmail.com>
2025-01-20 02:58:36 +0530

    Profile Page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,11 +1,10 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
+@section('content')
+
+
+    <div class="page-body">
+        <div class="container-xl my-auto">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
@@ -26,4 +25,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+
+@endsection
